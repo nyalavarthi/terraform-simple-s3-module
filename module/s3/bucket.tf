5aa3464f1e32c541bc294d32f27a250bf2ecb1ab
Narendra Yalavarthi <nyalavarthi@gmail.com>
2019-12-28 10:13:42 -0500

    Update bucket.tf

--- module/s3/bucket.tf
+++ module/s3/bucket.tf
@@ -1,9 +1,10 @@
-resource "aws_s3_bucket" "b" {
+resource "aws_s3_bucket" "sample-bucket" {
   bucket = "my-tf-test-bucket1-sandbox"
   acl    = "private"
 
-  /*tags = {
+  tags = {
     Name        = "My bucket"
     Environment = "Dev"
-  } */
+    Owner       = "NY"
+  }
 }
