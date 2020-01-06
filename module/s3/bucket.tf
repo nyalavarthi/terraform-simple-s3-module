70e8e03ef27f741ae7129149659130d101d6e525
Narendra Yalavarthi <nyalavarthi@gmail.com>
2020-01-06 14:02:01 -0500

    Update bucket.tf

--- module/s3/bucket.tf
+++ module/s3/bucket.tf
@@ -1,10 +1,31 @@
-resource "aws_s3_bucket" "sample-bucket" {
-  bucket = "my-tf-test-bucket1-sandbox"
-  acl    = "private"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-    Owner       = "NY"
+resource "aws_kms_key" "mykey" {
+   description             = "This key is used to encrypt bucket objects"
+   deletion_window_in_days = 10
+   tags = {
+    Name        = "My KMS Key"
+    Environment = "Sandbox"
+    Owner       = "Narendra Yala"
   }
+}
+
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket-4002"
+  acl    = "private"
+  
+  tags = {
+    Name        = "My S3 test bucket"
+    Environment = "Sandbox"
+    Owner       = "Narendra Yala",
+    DataType    = "Test files"  
+  }
+ 
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = "${aws_kms_key.mykey.arn}"
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+    
 }
